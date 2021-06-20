4aa8508cee5b6ee1a21336234ec44931100eef2a
Daniel Vargas <dvargas2013@gmail.com>
2021-06-20 03:06:20 -0400

    realized milestone's 1 oneliner could shave a few characters

--- Milestone_1/__oneliner.py
+++ Milestone_1/__oneliner.py
@@ -2,43 +2,49 @@
  pd=__import__("pandas"),
  sns=__import__("seaborn"),
  plt=__import__("matplotlib.pyplot").pyplot:
- (lambda covidnyc=pd.read_csv("../data-by-modzcta.csv"):
-  (lambda **kwargs: [f() for f in (
-      kwargs["borodeaths"], plt.figure,
-      kwargs["coviddeaths"], plt.figure,
-      kwargs["pctpos"], plt.figure,
-      kwargs["postests"], plt.figure,
-      kwargs["totalcases"], plt.figure,
-      kwargs["totaltests"], plt.show)])(
-      coviddeaths=lambda: [f() for f in (
-          (lambda: sns.histplot(data=covidnyc["COVID_DEATH_RATE"], kde=True)),
-          (lambda: plt.ylabel("Frequency")),
-          (lambda: plt.xlabel("Number of deaths from COVID-19 per neighborhood")),
-          (lambda: plt.title("Frequency of deaths from COVID-19 in each neighborhood")))],
-      postests=lambda: [f() for f in (
-          (lambda: sns.regplot(x="TOTAL_COVID_TESTS", y="PERCENT_POSITIVE", data=covidnyc)),
-          (lambda: plt.xlabel("Total number of COVID tests")),
-          (lambda: plt.ylabel("Percentage of positive COVID-19 tests")),
-          (lambda: plt.title("Scatter plot of total COVID-19 tests with percent of positive cases")))],
-      totalcases=lambda: [f() for f in (
-          (lambda: sns.histplot(data=covidnyc["COVID_CASE_COUNT"], kde=True)),
-          (lambda: plt.ylabel("Frequency")),
-          (lambda: plt.xlabel("Total number of COVID-19 cases per neighborhood")),
-          (lambda: plt.title("Frequency of total number of COVID-19 cases per neighborhood")))],
-      pctpos=lambda: [f() for f in (
-          (lambda: sns.histplot(data=covidnyc["PERCENT_POSITIVE"], kde=True)),
-          (lambda: plt.ylabel("Frequency")),
-          (lambda: plt.xlabel("Percent of residents per neighborhood tested positive for COVID-19")),
-          (lambda: plt.title(
-              "Frequency of percentages of residents in each neighborhood who tested positive for COVID-19")))],
-      totaltests=lambda: [f() for f in (
-          (lambda: sns.histplot(data=covidnyc["TOTAL_COVID_TESTS"], kde=True)),
-          (lambda: plt.ylabel("Frequency")),
-          (lambda: plt.xlabel("Total number of COVID-19 tests per neighborhood")),
-          (lambda: plt.title("Frequency of number of COVID-19 tests per neighborhood")))],
-      borodeaths=lambda: [f() for f in (
-          (lambda: sns.catplot(x="BOROUGH_GROUP", y="COVID_DEATH_COUNT",
-                               kind="box", data=covidnyc).fig.set_size_inches(6, 6)),
-          (lambda: plt.xlabel("Borough")),
-          (lambda: plt.title("Boxplot of deaths from COVID-19 by borough")),
-          (lambda: plt.ylabel("Number of deaths")))]))())()
+ (lambda
+  covidnyc=pd.read_csv("../data-by-modzcta.csv"),
+  pltlabel_and_title=(lambda ylabel, xlabel, title: [plt.ylabel(ylabel), plt.xlabel(xlabel), plt.title(title)]):
+  (lambda **kwargs: [
+      kwargs["borodeaths"](), plt.figure(),
+      kwargs["coviddeaths"](), plt.figure(),
+      kwargs["pctpos"](), plt.figure(),
+      kwargs["postests"](), plt.figure(),
+      kwargs["totalcases"](), plt.figure(),
+      kwargs["totaltests"](), plt.show()])(
+      coviddeaths=lambda: [
+          (sns.histplot(data=covidnyc["COVID_DEATH_RATE"], kde=True)),
+          (pltlabel_and_title(
+              "Frequency",
+              "Number of deaths from COVID-19 per neighborhood",
+              "Frequency of deaths from COVID-19 in each neighborhood"))],
+      postests=lambda: [
+          (sns.regplot(x="TOTAL_COVID_TESTS", y="PERCENT_POSITIVE", data=covidnyc)),
+          (pltlabel_and_title(
+              "Percentage of positive COVID-19 tests",
+              "Total number of COVID tests",
+              "Scatter plot of total COVID-19 tests with percent of positive cases"))],
+      totalcases=lambda: [
+          (sns.histplot(data=covidnyc["COVID_CASE_COUNT"], kde=True)),
+          (pltlabel_and_title(
+              "Frequency",
+              "Total number of COVID-19 cases per neighborhood",
+              "Frequency of total number of COVID-19 cases per neighborhood"))],
+      pctpos=lambda: [
+          (sns.histplot(data=covidnyc["PERCENT_POSITIVE"], kde=True)),
+          (pltlabel_and_title(
+              "Frequency",
+              "Percent of residents per neighborhood tested positive for COVID-19",
+              "Frequency of percentages of residents in each neighborhood who tested positive for COVID-19"))],
+      totaltests=lambda: [
+          (sns.histplot(data=covidnyc["TOTAL_COVID_TESTS"], kde=True)),
+          (pltlabel_and_title(
+              "Frequency",
+              "Total number of COVID-19 tests per neighborhood",
+              "Frequency of number of COVID-19 tests per neighborhood"))],
+      borodeaths=lambda: [
+          (sns.catplot(x="BOROUGH_GROUP", y="COVID_DEATH_COUNT", kind="box", data=covidnyc).fig.set_size_inches(6, 6)),
+          (pltlabel_and_title(
+              "Number of deaths",
+              "Borough",
+              "Boxplot of deaths from COVID-19 by borough"))]))())()
